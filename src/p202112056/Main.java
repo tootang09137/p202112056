76d436ae10c0be0d7e2b4aaf689dcae9c2d0657d
Lim minji <minjiim18@gmail.com>
2024-11-20 09:58:55 +0900

    four

--- src/p202112056/Main.java
+++ src/p202112056/Main.java
@@ -4,5 +4,11 @@ public class Main {
 	  public static void main(String[] args) {
 	    //  자신의 학번과 이름을 출력하라
 	    System.out.println("202112056 임민지");
+	    System.out.println(Hello.sum(10));
+	    System.out.println(Hello.sum(100));
+	    System.out.println(Hello.sum(1000));
+	    System.out.println(World.factorial(3));
+	    System.out.println(World.factorial(6));
+	    System.out.println(World.factorial(9));
 	  }
 	}
---
+++ src/p202112056/World.java
@@ -0,0 +1,11 @@
+package p202112056;
+
+public class World {
+
+	  public static long factorial(int n) {
+	    long result = 1;
+	    for (int i = 1; i <= n; ++i)
+	      result *= i;
+	    return result;
+	  }
+	}
